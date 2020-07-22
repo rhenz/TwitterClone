5ba0f322ff6ec9f91263c1a4eb1627750e586290
John Lawrence Salvador <john_salvador@ctl.creative.com>
2020-07-22 11:23:51 +0800

    Setup Project & Root View Controller programatically on SceneDelegate

---
+++ TwitterClone/Controller/Root View Controller/MainTabViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainTabViewController.swift
+//  TwitterClone
+//
+//  Created by Lawrence on 7/22/20.
+//  Copyright © 2020 JLCS. All rights reserved.
+//
+
+import UIKit
+
+class MainTabViewController: UITabBarController {
+
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+}
